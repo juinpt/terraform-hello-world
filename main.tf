f83bf1fa9e3a343eacce120e2c2aa1dda141a85c
Joao Afonso <joao@afonso.jp>
2025-05-22 13:38:00 +0900

    uploaded missing file

--- main.tf
+++ main.tf
@@ -3,24 +3,104 @@ provider aws {
   region = "ap-northeast-1"
 }
 
-resource "aws_instance" "aws_ubuntu" {
+module "web_instance" {
+  source = "./modules/ec2-instance"
+
   instance_type = "t2.micro"
   ami = "ami-0f415cc2783de6675"
   vpc_security_group_ids = [aws_security_group.demo_sg.id]
-  user_data = <<-EOF
-              #!/bin/bash
-              apt update -y
-              apt install -y nginx
-              systemctl enable nginx
-              systemctl start nginx
-              echo "<h1>Hello, world! NGINX is running on Ubuntu 22</h1>" > /var/www/html/index.html
-              EOF
+  instance_count = 2 
+  subnet_id           = data.aws_subnet.default_az1.id
+ 
+}
 
-  tags = {
-    Name = "HelloWorldWebServer"
+data "aws_subnet" "default_az1" {
+  filter {
+    name   = "vpc-id"
+    values = [aws_default_vpc.default.id]
   }
 
+  filter {
+    name   = "availability-zone"
+    values = ["ap-northeast-1a"]
+  }
 }
+
+data "aws_subnet" "default_az2" {
+  filter {
+    name   = "vpc-id"
+    values = [aws_default_vpc.default.id]
+  }
+
+  filter {
+    name   = "availability-zone"
+    values = ["ap-northeast-1c"]
+  }
+}
+
+resource "aws_security_group" "alb" {
+  name   = "alb-sg"
+  vpc_id = aws_default_vpc.default.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Allow HTTP from anywhere
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb" "app" {
+  name               = "my-app-lb"
+  load_balancer_type = "application"
+  internal           = false
+  security_groups    = [aws_security_group.alb.id]
+  subnets	     = [data.aws_subnet.default_az1.id, data.aws_subnet.default_az2.id]
+}
+
+
+resource "aws_lb_target_group" "app" {
+  name     = "my-app-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_default_vpc.default.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.app.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app.arn
+  }
+}
+
+resource "aws_lb_target_group_attachment" "app" {
+  count            = length(module.web_instance.instance_ids) 
+  target_group_arn = aws_lb_target_group.app.arn
+  target_id        = module.web_instance.instance_ids[count.index]
+  port             = 80
+}
+
 
 resource "aws_default_vpc" "default" {
 }
@@ -32,7 +112,7 @@ resource "aws_security_group" "demo_sg" {
 
 resource "aws_vpc_security_group_ingress_rule" "allow_80_ipv4" {
   security_group_id = aws_security_group.demo_sg.id
-  cidr_ipv4         = "0.0.0.0/0"
+  cidr_ipv4         = data.aws_subnet.default_az1.cidr_block
   from_port         = 80
   ip_protocol       = "tcp"
   to_port           = 80
@@ -40,7 +120,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_80_ipv4" {
 
 resource "aws_vpc_security_group_ingress_rule" "allow_22_ipiv4" {
   security_group_id = aws_security_group.demo_sg.id
-  cidr_ipv4         = "0.0.0.0/0"
+  cidr_ipv4         = data.aws_subnet.default_az1.cidr_block
   from_port         = 22
   ip_protocol       = "tcp"
   to_port           = 22
@@ -48,10 +128,6 @@ resource "aws_vpc_security_group_ingress_rule" "allow_22_ipiv4" {
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   security_group_id = aws_security_group.demo_sg.id
-  cidr_ipv4         = "0.0.0.0/0"
+  cidr_ipv4         = data.aws_subnet.default_az1.cidr_block
   ip_protocol       = "-1" # semantically equivalent to all ports
-}
-
-output "aws_instance_public_dns" {
-  value = aws_instance.aws_ubuntu.public_dns
 }
